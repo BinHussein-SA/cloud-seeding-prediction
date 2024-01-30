47a9bbfaed8417a5576c7c2e4b286469ff810b2f
BinHussein-SA <80439465+BinHussein-SA@users.noreply.github.com>
2024-01-30 20:50:31 +0300

    Update app_csp2.py

--- app_csp2.py
+++ app_csp2.py
@@ -12,13 +12,6 @@ from streamlit_option_menu import option_menu
 from PIL import Image
 from streamlit_extras.let_it_rain import rain
 
-# loading the saved models
-
-diabetes_model = pickle.load(open('saved/diabetes_model.sav', 'rb'))
-
-heart_disease_model = pickle.load(open('saved/heart_disease_model.sav', 'rb'))
-
-parkinsons_model = pickle.load(open('saved/parkinsons_model.sav', 'rb'))
 
 # page config function
 st.set_page_config(
@@ -32,8 +25,6 @@ st.set_page_config(
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
-# add kitten logo
-#logo = "https://drive.google.com/file/d/1USlyWZeb_gFbdGaNgCI89e7GwXDkJQSB/view?usp=sharing"
 
 def add_logoo(logo_path, width, height):
     """Read and return a resized logo"""
@@ -101,21 +92,17 @@ if selected == 'Cloud Seeding Program':
     
     
     # page title centered:
-    st.markdown("<h3 style='text-align: center; color: skyblue;'>البرنامج الإقليمي لإستمطار السحب</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: black;'>البرنامج الإقليمي لإستمطار السحب</h1>", unsafe_allow_html=True)
     #st.markdown("<h2 style='text-align: center; color: black;'>Smaller headline in black </h2>", unsafe_allow_html=True)
 
     # text under title centered
-    st.markdown("<h5 style='text-align: center; color: skyblue;'>هـو نـوع مـن عمليـات تعديـل الطقـس الـذي يهـدف إلــى تغييــر كميــة أو نــوع هطــول األمطــار الــذي يســقط مــن الســحب، وذلــك بنشــر وإطــاق أنــواع مختصــه مــن المــواد الكيميائيــة فــي الهــواء والتــي تسـاعد علـى تكاثـف السـحب أو النــوى الجليديـة، ممـا يغيــر العمليــات الفيزيائيــة الدقيقــة داخــل الســحابة. </h2>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: center; color: black;'>هـو نـوع مـن عمليـات تعديـل الطقـس الـذي يهـدف إلــى تغييــر كميــة أو نــوع هطــول األمطــار الــذي يســقط مــن الســحب، وذلــك بنشــر وإطــاق أنــواع مختصــه مــن المــواد الكيميائيــة فــي الهــواء والتــي تسـاعد علـى تكاثـف السـحب أو النــوى الجليديـة، ممـا يغيــر العمليــات الفيزيائيــة الدقيقــة داخــل الســحابة. </h2>", unsafe_allow_html=True)
 
     # page title
    # st.header('Cloud Seeding Program', divider='rainbow')
 
-    
-    st.markdown("*Hackathon* is **really** ***cool***.")
-    st.markdown(''':red[Cloud Seeding Program] :orange[can] :green[increase] :blue[the chance of rainning for some cloud.] :violet[for some]
-    :gray[regions in] :rainbow[Saudi Arabia].''')
-    st.markdown("Testing Emojis &mdash;\
-            🌧️:tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+
 
 
     
@@ -345,168 +332,6 @@ if selected == 'Informed Cloud Seeding Decision':
 #     st.success(diab_diagnosis)
 # =============================================================================
 
-# Farmer Warning Page
-if selected == 'Farmer Warning':
-
-    # page title
-    st.title('Farmer Warning using ML')
-
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        age = st.text_input('Age')
-
-    with col2:
-        sex = st.text_input('Sex')
-
-    with col3:
-        cp = st.text_input('Chest Pain types')
-
-    with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
-
-    with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
-
-    with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-
-    with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
-
-    with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
-
-    with col3:
-        exang = st.text_input('Exercise Induced Angina')
-
-    with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
-
-    with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
-
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-
-    with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-
-    # code for Prediction
-    heart_diagnosis = ''
-
-    # creating a button for Prediction
-
-    if st.button('Heart Disease Test Result'):
-
-        user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
-
-        user_input = [float(x) for x in user_input]
-
-        heart_prediction = heart_disease_model.predict([user_input])
-
-        if heart_prediction[0] == 1:
-            heart_diagnosis = 'The person is having heart disease'
-        else:
-            heart_diagnosis = 'The person does not have any heart disease'
-
-    st.success(heart_diagnosis)
-
-# Parkinson's Prediction Page
-if selected == "Healthcare Warning":
-
-    # page title
-    st.title("Healthcare Warning using ML")
-
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    with col1:
-        fo = st.text_input('MDVP:Fo(Hz)')
-
-    with col2:
-        fhi = st.text_input('MDVP:Fhi(Hz)')
-
-    with col3:
-        flo = st.text_input('MDVP:Flo(Hz)')
-
-    with col4:
-        Jitter_percent = st.text_input('MDVP:Jitter(%)')
-
-    with col5:
-        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-
-    with col1:
-        RAP = st.text_input('MDVP:RAP')
-
-    with col2:
-        PPQ = st.text_input('MDVP:PPQ')
-
-    with col3:
-        DDP = st.text_input('Jitter:DDP')
-
-    with col4:
-        Shimmer = st.text_input('MDVP:Shimmer')
-
-    with col5:
-        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
-
-    with col1:
-        APQ3 = st.text_input('Shimmer:APQ3')
-
-    with col2:
-        APQ5 = st.text_input('Shimmer:APQ5')
-
-    with col3:
-        APQ = st.text_input('MDVP:APQ')
-
-    with col4:
-        DDA = st.text_input('Shimmer:DDA')
-
-    with col5:
-        NHR = st.text_input('NHR')
-
-    with col1:
-        HNR = st.text_input('HNR')
-
-    with col2:
-        RPDE = st.text_input('RPDE')
-
-    with col3:
-        DFA = st.text_input('DFA')
-
-    with col4:
-        spread1 = st.text_input('spread1')
-
-    with col5:
-        spread2 = st.text_input('spread2')
-
-    with col1:
-        D2 = st.text_input('D2')
-
-    with col2:
-        PPE = st.text_input('PPE')
-
-    # code for Prediction
-    parkinsons_diagnosis = ''
-
-    # creating a button for Prediction    
-    if st.button("Healthcare Test Result"):
-
-        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
-                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
-                      APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
-
-        user_input = [float(x) for x in user_input]
-
-        parkinsons_prediction = parkinsons_model.predict([user_input])
-
-        if parkinsons_prediction[0] == 1:
-            parkinsons_diagnosis = "The person has Parkinson's disease"
-        else:
-            parkinsons_diagnosis = "The person does not have Parkinson's disease"
-
-    st.success(parkinsons_diagnosis)
-    
     
 footer="""<style>
 a:link , a:visited{
